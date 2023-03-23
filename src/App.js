6324c71d6e7621ac240f846981532d988c6d4e11
SonByeongJoo <thsqudwn05@gmail.com>
2023-03-23 10:38:34 +0900

    4번째 URL 변경

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 import './App.css';
 import UserVideoComponent from './UserVideoComponent';
 
-const APPLICATION_SERVER_URL = "https://i5a608.p.ssafy.io:8443";
+const APPLICATION_SERVER_URL = "https://drkko.site:8443";
 
 
 class App extends Component {
